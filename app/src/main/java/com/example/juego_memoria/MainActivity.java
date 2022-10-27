509b81e41a5e11734de44156423292983648a559
Sergio-Jurado <juradotrillosergio1207@gmail.com>
2022-10-27 17:47:35 +0200

    juego terminado

--- app/src/main/java/com/example/juego_memoria/MainActivity.java
+++ app/src/main/java/com/example/juego_memoria/MainActivity.java
@@ -1,130 +1,247 @@
 package com.example.juego_memoria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.ImageButton;
+import android.os.CountDownTimer;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import java.util.ArrayList;
+import android.widget.Toast;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8;
+    int cartas[]={R.drawable.azul, R.drawable.amarillo, R.drawable.gris, R.drawable.negro, R.drawable.rojo, R.drawable.rosa};
 
+    Integer[] idCarta = {0,0,1,1,2,2,3,3,4,4,5,5};
 
-    int imagenes[];
-    ImageButton [] botonera = new ImageButton[8];
-
-    int fondo;
-
-
-    ArrayList<Integer> arrayBarajado;
-
-    ImageButton primero;
-    int numeroPrimero, numeroSegundo;
-
-    boolean bloqueo = false;
+    int contador = 0;
+    int val;
+    int control =-1;
+    Bitmap bmp;
+    boolean b = true;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cargarImagenes();
         iniciar();
     }
 
-    public void cargarImagenes(){
-        imagenes = new int[]{
-                R.drawable.opcion1,
-                R.drawable.opcion2,
-                R.drawable.opcion3,
-                R.drawable.opcion4,
-        };
+    public void iniciar(){
+        List<Integer> list = Arrays.asList(idCarta);
+        Collections.shuffle(list);
+        list.toArray(idCarta);
 
-        fondo = R.drawable.back;
+        ImageView c1 = findViewById(R.id.carta1);
+        ImageView c2 = findViewById(R.id.carta2);
+        ImageView c3 = findViewById(R.id.carta3);
+        ImageView c4 = findViewById(R.id.carta4);
+        ImageView c5 = findViewById(R.id.carta5);
+        ImageView c6 = findViewById(R.id.carta6);
+        ImageView c7 = findViewById(R.id.carta7);
+        ImageView c8 = findViewById(R.id.carta8);
+        ImageView c9 = findViewById(R.id.carta9);
+        ImageView c10 = findViewById(R.id.carta10);
+        ImageView c11 = findViewById(R.id.carta11);
+        ImageView c12 = findViewById(R.id.carta12);
+
+        c1.setImageResource(R.drawable.back);
+        c2.setImageResource(R.drawable.back);
+        c3.setImageResource(R.drawable.back);
+        c4.setImageResource(R.drawable.back);
+        c5.setImageResource(R.drawable.back);
+        c6.setImageResource(R.drawable.back);
+        c7.setImageResource(R.drawable.back);
+        c8.setImageResource(R.drawable.back);
+        c9.setImageResource(R.drawable.back);
+        c10.setImageResource(R.drawable.back);
+        c11.setImageResource(R.drawable.back);
+        c12.setImageResource(R.drawable.back);
+
+        c1.setEnabled(true);
+        c2.setEnabled(true);
+        c3.setEnabled(true);
+        c4.setEnabled(true);
+        c5.setEnabled(true);
+        c6.setEnabled(true);
+        c7.setEnabled(true);
+        c8.setEnabled(true);
+        c9.setEnabled(true);
+        c10.setEnabled(true);
+        c11.setEnabled(true);
+        c12.setEnabled(true);
     }
 
-    public ArrayList<Integer> barajar(int longitud) {
-        ArrayList resultadoA = new ArrayList<Integer>();
-        for(int i=0; i<longitud; i++)
-            resultadoA.add(i % longitud/2);
-        Collections.shuffle(resultadoA);
-        return  resultadoA;
+    public void vuelta1(View vista){
+        if(b){
+            val=R.id.carta1;
+        }
+        ImageView c1 = findViewById(R.id.carta1);
+        controlador(idCarta[0],c1);
     }
 
-    public void cargarBotones(){
-        carta1 = (ImageButton) findViewById(R.id.carta1);
-        botonera[0] = carta1;
-        carta2 = (ImageButton) findViewById(R.id.carta2);
-        botonera[1] = carta2;
-        carta3 = (ImageButton) findViewById(R.id.carta3);
-        botonera[3] = carta3;
-        carta4 = (ImageButton) findViewById(R.id.carta4);
-        botonera[4] = carta4;
-        carta5 = (ImageButton) findViewById(R.id.carta5);
-        botonera[5] = carta5;
-        carta6 = (ImageButton) findViewById(R.id.carta6);
-        botonera[6] = carta6;
-        carta7 = (ImageButton) findViewById(R.id.carta7);
-        botonera[7] = carta7;
-        carta8 = (ImageButton) findViewById(R.id.carta8);
-        botonera[8] = carta8;
-
+    public void vuelta2(View vista){
+        if(b){
+            val=R.id.carta2;
+        }
+        ImageView c2 = findViewById(R.id.carta2);
+        controlador(idCarta[1],c2);
     }
-    public void comprobar(int i, final ImageButton imgb){
-        if(primero==null){
-            primero = imgb;
+    public void vuelta3(View vista){
+        if(b){
+            val=R.id.carta3;
+        }
+        ImageView c3 = findViewById(R.id.carta3);
+        controlador(idCarta[2],c3);
+    }
+    public void vuelta4(View vista){
+        if(b){
+            val=R.id.carta4;
+        }
+        ImageView c4 = findViewById(R.id.carta4);
+        controlador(idCarta[3],c4);
+    }
+    public void vuelta5(View vista){
+        if(b){
+            val=R.id.carta5;
+        }
+        ImageView c5 = findViewById(R.id.carta5);
+        controlador(idCarta[4],c5);
+    }
+    public void vuelta6(View vista){
+        if(b){
+            val=R.id.carta6;
+        }
+        ImageView c6 = findViewById(R.id.carta6);
+        controlador(idCarta[5],c6);
+    }
+    public void vuelta7(View vista){
+        if(b){
+            val=R.id.carta7;
+        }
+        ImageView c7 = findViewById(R.id.carta7);
+        controlador(idCarta[6],c7);
+    }
+    public void vuelta8(View vista){
+        if(b){
+            val=R.id.carta8;
+        }
+        ImageView c8 = findViewById(R.id.carta8);
+        controlador(idCarta[7],c8);
+    }
+    public void vuelta9(View vista){
+        if(b){
+            val=R.id.carta9;
+        }
+        ImageView c9 = findViewById(R.id.carta9);
+        controlador(idCarta[8],c9);
+    }
+    public void vuelta10(View vista){
+        if(b){
+            val=R.id.carta10;
+        }
+        ImageView c10 = findViewById(R.id.carta10);
+        controlador(idCarta[9],c10);
+    }
+    public void vuelta11(View vista){
+        if(b){
+            val=R.id.carta11;
+        }
+        ImageView c11 = findViewById(R.id.carta11);
+        controlador(idCarta[10],c11);
+    }
+    public void vuelta12(View vista){
+        if(b){
+            val=R.id.carta12;
+        }
+        ImageView c12 = findViewById(R.id.carta12);
+        controlador(idCarta[11],c12);
+    }
 
-            primero.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            primero.setImageResource(imagenes[arrayBarajado.get(i)]);
-            primero.setEnabled(false);
+    private void controlador(int i, final ImageView img){
+        final ImageView p = findViewById(val);
 
-            numeroPrimero=arrayBarajado.get(i);
+        bmp = null;
+        switch (i){
+            case 0:
+                bmp = BitmapFactory.decodeResource(getResources(), R.drawable.azul);
+            break;
+
+            case 1:
+                bmp = BitmapFactory.decodeResource(getResources(), R.drawable.amarillo);
+            break;
+
+            case 2:
+                bmp = BitmapFactory.decodeResource(getResources(), R.drawable.gris);
+            break;
+
+            case 3:
+                bmp = BitmapFactory.decodeResource(getResources(), R.drawable.rojo);
+            break;
+
+            case 4:
+                bmp = BitmapFactory.decodeResource(getResources(), R.drawable.negro);
+            break;
+
+            case 5:
+                bmp = BitmapFactory.decodeResource(getResources(), R.drawable.rosa);
+            break;
+        }
+
+        if(control == -1){
+            control = i;
+            img.setImageBitmap(bmp);
+            img.setEnabled(false);
+            b= false;
         }else{
-            bloqueo=true;
-            imgb.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imgb.setImageResource(imagenes[arrayBarajado.get(i)]);
-            imgb.setEnabled(false);
-            numeroSegundo=arrayBarajado.get(i);
-            if(numeroPrimero==numeroSegundo){
-                primero=null;
-                bloqueo=false;
-
+            if(control == i){
+                img.setImageBitmap(bmp);
+                img.setEnabled(false);
+                control = -1;
+                b= true;
+                Toast.makeText(this, "bien", Toast.LENGTH_LONG).show();
+                contador++;
             }else{
-                primero.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                primero.setImageResource(R.drawable.back);
-                imgb.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                imgb.setImageResource(R.drawable.back);
-                primero.setEnabled(true);
-                imgb.setEnabled(true);
-                primero = null;
-                bloqueo = false;
-                }
+                b  = true;
+                control = -1;
+
+                new CountDownTimer(2000,1000){
+                    @Override
+                    public void onTick(long arg0){
+                        img.setImageBitmap(bmp);
+                    }
+
+                    @Override
+                    public void onFinish(){
+                        p.setImageResource(R.drawable.back);
+                        img.setImageResource(R.drawable.back);
+                        p.setEnabled(true);
+                        img.setEnabled(true);
+                    }
+                }.start();
+                Toast.makeText(this, "Mal", Toast.LENGTH_LONG).show();
             }
         }
 
-    public void iniciar(){
-        arrayBarajado = barajar(imagenes.length*2);
-        cargarBotones();
+        if (contador == 6){
+            Toast.makeText(this, "Así me gusta", Toast.LENGTH_LONG).show();
+            new CountDownTimer(3000,1000){
 
-        for(int i=0; i<botonera.length; i++) {
-            botonera[i].setScaleType(ImageView.ScaleType.CENTER_CROP);
-            botonera[i].setImageResource(imagenes[arrayBarajado.get(i)]);
-        }
-
-
-        for(int i=0; i <arrayBarajado.size(); i++){
-            final int j=i;
-            botonera[i].setEnabled(true);
-            botonera[i].setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) {
-                    if(!bloqueo)
-                        comprobar(j, botonera[j]);
+                public void onTick(long arg0){
+
                 }
-            });
+
+                @Override
+                public void onFinish(){
+                    iniciar();
+                }
+            }.start();
         }
     }
+
 }
